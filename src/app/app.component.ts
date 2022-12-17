e3cf7c36f9daf73fd3e2fb63f265471bc25da14d
Roop Sagarh Talari <talariroopsagar@gmail.com>
2022-12-18 01:31:27 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'drag_and_drop';
+  reds = ['ball', 'bat', 'tennis'];
+  blues = ['cricket', 'sports', 'football', 'NBA'];
+  greens = ['worldcup', 'IPL'];
+  blacks = ['FIFA', 'India', 'Hockey'];
+
+  delete(stack: any, index: any) {
+    if (stack === 'red') this.reds.splice(index, 1);
+    if (stack === 'blue') this.blues.splice(index, 1);
+    if (stack === 'green') this.greens.splice(index, 1);
+    if (stack === 'black') this.blacks.splice(index, 1);
+  }
+
+  add(stack: any) {
+    if (stack.length < 8) {
+      stack.push('New Item');
+    } else {
+      alert('Maximum slot limit reached');
+    }
+  }
+  
+  drop(event: CdkDragDrop<string[]>, data: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else if (data.length < 8) {
+      {
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+      }
+    } else alert('Maximum slot limit reached')
+  }
 }
